39470370f90b70f61a2e8e8430f9c4b2c31a3ba4
pcpasapas <pcpasapas@gmail.com>
2022-11-08 15:25:13 +0100

    'build'

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-    server: { https: true },
     plugins: [
-        basicSsl,
+
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
