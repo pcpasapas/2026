7f573120006b9b5f35dc0d5e1e03d6efa827e29e
pcpasapas <pcpasapas@gmail.com>
2022-11-08 15:00:45 +0100

    'build'

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
+    server: { https: true },
     plugins: [
+        mkcert,
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
