07c2c0a21882262ae848d69364ad61c40f4a1f63
pcpasapas <pcpasapas@gmail.com>
2022-11-09 10:45:16 +0100

    'build'

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,7 @@ window._ = _;
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpsRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
