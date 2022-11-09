02d57f5bb850d4586b2ffdbe11a588cca9b529fc
pcpasapas <pcpasapas@gmail.com>
2022-11-09 10:34:01 +0100

    'build'

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 export default defineConfig({
+    server: {
+        https: true,
+        host: '127.0.0.1',
+        hmr: {
+            host: 'localhost'
+        },
+    },
     plugins: [
+        basicSsl(),
 
         laravel({
             input: 'resources/js/app.js',
